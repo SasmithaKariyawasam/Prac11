cbd8f48cc6567e0cb5d861d35562637a1f605d2c
Sasmitha Kariyawasam <Sasamavikariyawasam12@gmail.com>
2024-10-19 13:52:45 +0530

    Add files via upload

---
+++ prss.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int width;
+    int height;
+    cout<<"Enter the width of the Rectangle: "<<endl;
+    cin>>width;
+    cout<<"Enter the height of the Rectangle: "<<endl;
+    cin>>height;
+    int area;
+    area = height*width;
+    cout<<"Area of the Rectangle is: "<<area<<endl;
+    return 0;
+}
